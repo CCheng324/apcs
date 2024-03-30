88959feeecd56930358ca3ff39d98ea086e70582
CCheng324 <76075charlie@gmail.com>
2024-03-30 08:34:45 +0800

    basic

--- basic/a004.py
+++ basic/a004.py
@@ -1,6 +1,9 @@
-n=int(input())
-
-if n%400==0 or( (n%4)==0 and (n%100)!=0):
-    print("閏年")
-else:
-    print("平年")
+while True:
+    try:
+        a=int(input())
+        if (a%4==0 and a%100!=0) or a%400==0:
+            print("閏年")
+        else:
+            print("平年")
+    except EOFError:
+        break
