e19310bf1b39ffae005898a0116a59d563e02d11
CCheng324 <76075charlie@gmail.com>
2024-04-12 01:06:59 +0800

    err

--- 猜拳.py
+++ 猜拳.py
@@ -1,37 +1,36 @@
-x_win=False
-y_win=False
-def w_or_f(x,y):
- if x-y<0:
-  x_win==True
- else:
-  y_win==True
+browin=False
+siswin=False
 #input()--------------------------
 F=int(input())
 N=int(input())
 sis=[int(a) for a in input().split()]
 #---------------------------------
 
-f=[F]
+
+
 for k in range(N):
-  w_or_f(F,sis[k])
- 
+  print(F,end=' ')
+  if (F==0 and sis[k]==2) or (F==2 and sis[k]==5) or (F==5 and sis[k]==0):
+    print(f': Won at round {k+1}')
+    browin=True
+    break
+  elif (F==2 and sis[k]==0) or (F==5 and sis[k]==2) or (F==0 and sis[k]==5):
+    print(f': Lost at round {k+1}')
+    siswin=True
+    break
+
 #判斷下次出拳(哥)-------------------
   if sis[k]==sis[k-1]:
     if sis[k]==0:
-      F==5
-      f.append(5)
+      F=5
     if sis[k]==2:
-      F==0
-      f.append(0)
+      F=0
     if sis[k]==5:
-      F==2
-      f.append(2)
+      F=2
   else:
-    F==sis[k]
+    F=sis[k]
 #----------------------------------
-  if x_win==True:
-    print(f'{} : Won at round {k+1}')
-  elif y_win==True:
-    print(f'{} : Lost at round {k+1}')
-if x_win==False and y_win==False:
-  print(f'{} : Drew at round {k+1}')
+
+if browin==False and siswin==False:
+  print(f': Drew at round {k+1}')
+
