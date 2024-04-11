09814f9cc31bf306cec8453eb3779d86cc887538
CCheng324 <140939917+CCheng324@users.noreply.github.com>
2024-04-11 14:29:35 +0800

    {} 輸出解決

--- 猜拳.py
+++ 猜拳.py
@@ -5,12 +5,16 @@ def w_or_f(x,y):
   x_win==True
  else:
   y_win==True
+#input()--------------------------
+F=int(input())
+N=int(input())
+sis=[int(a) for a in input().split()]
+#---------------------------------
 
-for k in range(int(input())):
-  F=int(input())
-  f=[F]
-  sis=[int(a) for a in input().split()]
+f=[F]
+for k in range(N):
   w_or_f(F,sis[k])
+ 
 #判斷下次出拳(哥)-------------------
   if sis[k]==sis[k-1]:
     if sis[k]==0:
@@ -24,13 +28,10 @@ for k in range(int(input())):
       f.append(2)
   else:
     F==sis[k]
-#-------------------------------------
-print(*f,sep="")
-'''
+#----------------------------------
   if x_win==True:
     print(f'{} : Won at round {k+1}')
   elif y_win==True:
     print(f'{} : Lost at round {k+1}')
 if x_win==False and y_win==False:
   print(f'{} : Drew at round {k+1}')
-'''
