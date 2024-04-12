3fcebab597406800bb7b6257af36726d369ab273
CCheng324 <76075charlie@gmail.com>
2024-04-12 18:44:20 +0800

    err75%

--- 猜拳.py
+++ 猜拳.py
@@ -1,36 +1,34 @@
-browin=False
-siswin=False
-#input()--------------------------
-F=int(input())
-N=int(input())
+#input------------------------
+f=int(input())
+n=int(input())
 sis=[int(a) for a in input().split()]
-#---------------------------------
+ #----------------------------
+someone_win=False
 
+for i in range(n):
+    print(f,end=" ")
+    #browin----------
+    if (f==0 and sis[i]==2) or (f==2 and sis[i]==5) or (f==5 and sis[i]==0):
+        print(f': Won at round {i+1}')
+        someone_win=True
+        break
+    #siswin----------
+    elif (f==2 and sis[i]==0) or(f==5 and sis[i]==2) or (f==0 and sis[i]==5):
+        print(f': Lost at round {i+1}')
+        someone_win=True
+        break               
+    
+    #判斷出拳---------
+    if sis[i]==sis[i-1]:
+        if sis[i]==0:
+            f=5
+        elif sis[i]==2:
+            f=0
+        elif sis[i]==5:
+            f=2
+    else:
+        f=sis[i]
 
-
-for k in range(N):
-  print(F,end=' ')
-  if (F==0 and sis[k]==2) or (F==2 and sis[k]==5) or (F==5 and sis[k]==0):
-    print(f': Won at round {k+1}')
-    browin=True
-    break
-  elif (F==2 and sis[k]==0) or (F==5 and sis[k]==2) or (F==0 and sis[k]==5):
-    print(f': Lost at round {k+1}')
-    siswin=True
-    break
-
-#判斷下次出拳(哥)-------------------
-  if sis[k]==sis[k-1]:
-    if sis[k]==0:
-      F=5
-    if sis[k]==2:
-      F=0
-    if sis[k]==5:
-      F=2
-  else:
-    F=sis[k]
-#----------------------------------
-
-if browin==False and siswin==False:
-  print(f': Drew at round {k+1}')
+if not(someone_win):
+    print(f': Drew at round {i+1}')
 
